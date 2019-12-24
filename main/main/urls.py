0df4fcc56ed6682a022f004fb2e3509492f0f971
Johnnatan Barros <johnnatan.barros@aegea.com.br>
2019-12-24 10:19:12 -0400

    Add Home Page View

--- main/main/urls.py
+++ main/main/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blog.views import HomePageView
+
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
