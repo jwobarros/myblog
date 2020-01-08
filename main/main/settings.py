40d8237c8a1fddb768ac68b9ed895e0c8fb2ce98
Johnnatan Barros <johnnatan.barros@aegea.com.br>
2020-01-08 10:20:10 -0400

    Added Heroku configs

--- main/main/settings.py
+++ main/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'crispy_forms',
     'blog'
 ]
 
@@ -122,4 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# CRISPY FORMS SETTINGS
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+# Heroku config
+django_heroku.settings(locals())
