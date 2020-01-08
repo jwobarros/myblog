9ca9e74a8c453f12dceea5351c8e39defd75354f
Johnnatan Barros <johnnatan.barros@aegea.com.br>
2020-01-08 10:23:40 -0400

    URLs routes

--- main/main/urls.py
+++ main/main/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
-from blog.views import HomePageView
+from blog.views import HomePageView, post_detail, subscribe_view
+
+
+admin.site.site_header = 'Administração do site'
+admin.site.index_title = 'Johnnatan Barros'
+admin.site.site_title = 'Administração'
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('post/<slug:slug>/', post_detail, name='post-detail'),
+    path('subscribe', subscribe_view, name='subscribe'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
