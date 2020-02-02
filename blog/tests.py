6cb8d7c2c0b876fb13d5e39f710b34f9a6f05342
Johnnatan Barros <johnnatanwillian@hotmail.com>
2020-02-02 19:22:10 -0400

    Updated tests using model mommy

--- blog/tests.py
+++ blog/tests.py
@@ -1,83 +1,57 @@
 from django.test import TestCase
+from model_mommy import mommy   
 from blog.models import Post, Tag, Comment, Subscriber
 # Create your tests here.
 
 class TagTestCase(TestCase):
     def setUp(self):
-        Tag.objects.create(name="Django")
-        Tag.objects.create(name="Python")
+        self.django_tag = mommy.make(Tag, name="Django")
+        self.python_tag = mommy.make(Tag, name="Python")
 
     def test_tag_creation(self):
         """Test Tag creation"""
-        django = Tag.objects.get(name="Django")
-        python = Tag.objects.get(name="Python")
-        self.assertEqual(django.__str__(), 'Django')
-        self.assertEqual(python.__str__(), 'Python')
+        self.assertEqual(self.django_tag.__str__(), 'Django')
+        self.assertEqual(self.python_tag.__str__(), 'Python')
 
 
 class PostTestCase(TestCase):
     def setUp(self):
-        Tag.objects.create(name="Django")
-        django_tag = Tag.objects.get(name="Django")
-        Post.objects.create(
-            title="Django",
-            summary="Como iniciar seus projetos usando Django.",
-            image="C:\\Users\\johnnatan.barros\\Pictures\\IMG_0628.jpeg",
-            content="Como iniciar seus projetos usando Django.",
-        ).tags.set([django_tag])
-
+        self.tag = mommy.make(Tag, name="Django")
+        self.post = mommy.make(Post, title="Django project")
+        self.post.tags.set([self.tag])
+    
     def test_post_creation(self):
         """Test post creation"""
-        django_post = Post.objects.get(title="Django")
-        self.assertEqual(django_post.title, 'Django')
-        self.assertEqual(django_post.summary, 'Como iniciar seus projetos usando Django.')
-        self.assertEqual(django_post.content, 'Como iniciar seus projetos usando Django.')
-
+        self.assertEqual(self.post.title, 'Django project')
 
 class CommentTestCase(TestCase):
     def setUp(self):
-        Tag.objects.create(name="Django")
-        Tag.objects.create(name="Python")
-
-        django_tag = Tag.objects.get(name="Django")
-        python_tag = Tag.objects.get(name="Python")
-
-        Post.objects.create(
-            title="Django",
-            summary="Como iniciar seus projetos usando Django.",
-            image="C:\\Users\\johnnatan.barros\\Pictures\\IMG_0628.jpeg",
-            content="Como iniciar seus projetos usando Django.",
-        ).tags.set([django_tag, python_tag])
-
-        post = Post.objects.get(title="Django")
-        
-        Comment.objects.create(
-            post=post,
+        self.tag = mommy.make(Tag, name="Django")
+        self.post = mommy.make(Post, title="Django")
+        self.post.tags.set([self.tag])
+        self.comment = mommy.make(Comment, 
+            post=self.post,
             name="Johnnatan Barros",
             email="johnnatan.barros@outlook.com",
             body="Testing comment model."
         )
-        
 
     def test_comments_creation(self):
         """Test Tag creation"""
-        comment = Comment.objects.get(name="Johnnatan Barros")        
-        self.assertEqual(comment.name, 'Johnnatan Barros')
-        self.assertEqual(comment.email, 'johnnatan.barros@outlook.com')
-        self.assertEqual(comment.body, 'Testing comment model.')
+        self.assertEqual(self.comment.name, 'Johnnatan Barros')
+        self.assertEqual(self.comment.email, 'johnnatan.barros@outlook.com')
+        self.assertEqual(self.comment.body, 'Testing comment model.')
 
 
 class SubscriberTestCase(TestCase):
     def setUp(self):
-        Subscriber.objects.create(email="johnnatan.barros@outlook.com")
+        self.subscriber = mommy.make(Subscriber, email="johnnatan.barros@outlook.com")
 
     def test_subscription(self):
         """Test subscription"""
-        subscriber = Subscriber.objects.get(email="johnnatan.barros@outlook.com")
-        self.assertEqual(subscriber.active, True)
+        self.assertEqual(self.subscriber.active, True)
 
     def test_unsubscription(self):
         """Test unsubscription"""
-        subscriber = Subscriber.objects.get(email="johnnatan.barros@outlook.com")
-        subscriber.unsubscribe()
-        self.assertEqual(subscriber.active, False)
+        self.subscriber.unsubscribe()
+        self.assertEqual(self.subscriber.active, False)
